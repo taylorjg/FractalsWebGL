e02703fcbaa1d7c1af46228a0fc6e1fb8d743056
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-03-28 00:44:56 +0100

    es2015-ified the js code

--- client/index.js
+++ client/index.js
@@ -1,74 +1,58 @@
 import vertexShaderSource from './shader.vert.glsl';
 import fragmentShaderSource from './shader.frag.glsl';
 
-var gl;
-var aVertexPosition;
-var aPlotPosition;
-var shaderProgram;
-var aVertexPosition;
-var centerOffsetX = 0;
-var centerOffsetY = 0;
-var zoom;
-var zoomCenterX;
-var zoomCenterY;
-var vertexPositionBuffer;
-var baseCorners = [
-    [0.7, 1.2],
-    [-2.2, 1.2],
-    [0.7, -1.2],
-    [-2.2, -1.2],
-];
+let gl;
+let aVertexPosition;
+let aPlotPosition;
+let vertexPositionBuffer;
 
-function initGL(canvas) {
+const initGL = canvas => {
     try {
-        gl = canvas.getContext("webgl");
+        gl = canvas.getContext('webgl');
         gl.viewportWidth = canvas.width;
         gl.viewportHeight = canvas.height;
-    } catch (e) {
+    }
+    catch (e) {
+        console.error(`ERROR: ${e.message}`);
     }
     if (!gl) {
-        alert("Could not initialise WebGL, sorry :-(");
+        console.error('Failed to initialise WebGL');
     }
-}
+};
 
-function getShader(gl, source, type) {
-    var shader = gl.createShader(type);
+const getShader = (gl, source, shaderType) => {
+    const shader = gl.createShader(shaderType);
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        alert(gl.getShaderInfoLog(shader));
+        console.error(gl.getShaderInfoLog(shader));
         return null;
     }
     return shader;
 }
 
-function initShaders() {
-
-    var fragmentShader = getShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER);
-    var vertexShader = getShader(gl, vertexShaderSource, gl.VERTEX_SHADER);
-
-    shaderProgram = gl.createProgram();
+const initShaders = () => {
+    const vertexShader = getShader(gl, vertexShaderSource, gl.VERTEX_SHADER);
+    const fragmentShader = getShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER);
+    const shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
-
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert("Could not initialise shaders");
+        console.error('Could not initialise shaders');
+        return;
     }
-
     gl.useProgram(shaderProgram);
-
-    aVertexPosition = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    aVertexPosition = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
     gl.enableVertexAttribArray(aVertexPosition);
-
-    aPlotPosition = gl.getAttribLocation(shaderProgram, "aPlotPosition");
+    aPlotPosition = gl.getAttribLocation(shaderProgram, 'aPlotPosition');
     gl.enableVertexAttribArray(aPlotPosition);
 }
 
-function initBuffers() {
+const initBuffers = () => {
     vertexPositionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-    var vertices = [
+    const vertices = [
         1.0, 1.0,
         -1.0, 1.0,
         1.0, -1.0,
@@ -79,41 +63,36 @@ function initBuffers() {
     vertexPositionBuffer.numItems = 4;
 }
 
-function drawScene() {
+const drawScene = () => {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
     gl.vertexAttribPointer(aVertexPosition, vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
-    var plotPositionBuffer = gl.createBuffer();
+    const plotPositionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, plotPositionBuffer);
-    var cornerIx;
-    var corners = [];
-    for (cornerIx in baseCorners) {
-        var x = baseCorners[cornerIx][0];
-        var y = baseCorners[cornerIx][1];
-        corners.push(x / zoom + centerOffsetX);
-        corners.push(y / zoom + centerOffsetY);
-    }
 
+    const baseCorners = [
+        [0.7, 1.2],
+        [-2.2, 1.2],
+        [0.7, -1.2],
+        [-2.2, -1.2]
+    ];
+    const corners = [];
+    for (const i in baseCorners) {
+        var x = baseCorners[i][0];
+        var y = baseCorners[i][1];
+        corners.push(x);
+        corners.push(y);
+    }
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(corners), gl.STATIC_DRAW);
     gl.vertexAttribPointer(aPlotPosition, 2, gl.FLOAT, false, 0, 0);
-
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
     gl.deleteBuffer(plotPositionBuffer)
 }
 
-function resetZoom() {
-    zoom = 1.0;
-    zoomCenterX = 0.28693186889504513;
-    zoomCenterY = 0.014286693904085048;
-}
-
-function webGLStart() {
-    resetZoom();
-    var canvas = document.getElementById("canvas");
+const start = () => {
+    var canvas = document.getElementById('canvas');
     initGL(canvas);
     initShaders()
     initBuffers();
@@ -121,4 +100,4 @@ function webGLStart() {
     drawScene();
 }
 
-webGLStart();
+start();
