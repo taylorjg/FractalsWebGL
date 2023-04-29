837b482b5b42f35618d0c24f22bd972ec6d4463b
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-04-29 22:19:01 +0100

    Update eslint

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
 
   // Needed because of the use of "import.meta.url" when calling "new Worker()".
   // https://stackoverflow.com/questions/54337576/eslint-import-meta-causes-fatal-parsing-error
-  parser: "babel-eslint",
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    requireConfigFile: false
+  },
 
   env: {
     browser: true,
