c5ae0de08b26fa8b4dbd73b2b91c0ef14bf10bae
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-05-07 11:54:17 +0100

    Tweak how we lookup colour map index in WebGL 1

--- src/shaders/webgl1/loop.glsl
+++ src/shaders/webgl1/loop.glsl
@@ -1,6 +1,8 @@
 // Same value as INITIAL_ITERATIONS in constants.js
 const int MAX_ITERATIONS = 128;
 
+const int COLOUR_MAP_SIZE = 256;
+
 vec4 loop(sampler2D colourMap, vec2 z, vec2 c) {
   int iteration = MAX_ITERATIONS;
   for (int i = 0; i < MAX_ITERATIONS; i++) {
@@ -11,8 +13,12 @@ vec4 loop(sampler2D colourMap, vec2 z, vec2 c) {
     vec2 zSquared = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y);
     z = zSquared + c;
   }
-  float s = float(iteration) / float(MAX_ITERATIONS + 1);
-  float t = 0.0;
-  vec2 coord = vec2(s, t);
-  return texture2D(colourMap, coord);
+
+  int colourMapIndex = (COLOUR_MAP_SIZE - 1) * iteration / MAX_ITERATIONS;
+  float colourMapEntryMidPoint = 1.0 / float(COLOUR_MAP_SIZE) / 2.0;
+  float n = float(colourMapIndex);
+  float s = (2.0 * n + 1.0) * colourMapEntryMidPoint;
+  float t = 0.5;
+  vec2 textureCoords = vec2(s, t);
+  return texture2D(colourMap, textureCoords);
 }
--- src/shaders/webgl2/loop.glsl
+++ src/shaders/webgl2/loop.glsl
@@ -1,3 +1,5 @@
+const int COLOUR_MAP_SIZE = 256;
+
 vec4 loop(int maxIterations, sampler2D colourMap, vec2 z, vec2 c) {
   int iteration = 0;
   while (iteration < maxIterations) {
@@ -6,9 +8,10 @@ vec4 loop(int maxIterations, sampler2D colourMap, vec2 z, vec2 c) {
     z = zSquared + c;
     iteration++;
   }
-  int s = 255 * iteration / maxIterations;
+
+  int s = (COLOUR_MAP_SIZE - 1) * iteration / maxIterations;
   int t = 0;
-  ivec2 coord = ivec2(s, t);
-  int lod = 0;
-  return texelFetch(colourMap, coord, lod);
+  ivec2 textureCoords = ivec2(s, t);
+  int levelOfDetail = 0;
+  return texelFetch(colourMap, textureCoords, levelOfDetail);
 }
